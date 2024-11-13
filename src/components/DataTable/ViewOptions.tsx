40d4fba0df7d4fc9907ce2adbbc833bd308c78cb
Jay Ganatra <ganatrajay2000@gmail.com>
2024-11-12 23:55:01 -0500

    remove mobile responsiveness

--- src/components/DataTable/ViewOptions.tsx
+++ src/components/DataTable/ViewOptions.tsx
@@ -13,7 +13,6 @@ import {
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
 import { snakeToTitle } from "@/lib/strManipulation";
-import { shouldHideOnMobile } from "@/lib/constants";
 
 interface DataTableViewOptionsProps<TData> {
   table: Table<TData>;
@@ -22,9 +21,6 @@ interface DataTableViewOptionsProps<TData> {
 export function DataTableViewOptions<TData>({
   table,
 }: DataTableViewOptionsProps<TData>) {
-  const [columnVisibility, setColumnVisibility] = useState<
-    Record<string, boolean>
-  >({});
   const [currentTime, setCurrentTime] = useState(new Date());
 
   useEffect(() => {
@@ -34,30 +30,6 @@ export function DataTableViewOptions<TData>({
 
     return () => clearInterval(timer);
   }, []);
-
-  useEffect(() => {
-    // Initialize column visibility based on screen size
-    const isSmallScreen = window.innerWidth < 1024;
-    const initialVisibility = table.getAllColumns().reduce((acc, column) => {
-      // Hide specified columns on small screens
-      shouldHideOnMobile.includes(column.id);
-      acc[column.id] = isSmallScreen ? !shouldHideOnMobile : true;
-      handleVisibilityChange(column.id, acc[column.id]);
-      return acc;
-    }, {} as Record<string, boolean>);
-    initialVisibility.higher = false;
-    setColumnVisibility(initialVisibility);
-    table.setColumnVisibility(initialVisibility);
-  }, []);
-
-  const handleVisibilityChange = (columnId: string, visible: boolean) => {
-    const newVisibility = {
-      ...columnVisibility,
-      [columnId]: visible,
-    };
-    setColumnVisibility(newVisibility);
-    table.setColumnVisibility(newVisibility);
-  };
 
   return (
     <div className="lg:flex items-center lg:space-x-4">
@@ -89,10 +61,8 @@ export function DataTableViewOptions<TData>({
               <DropdownMenuCheckboxItem
                 key={column.id}
                 className="capitalize"
-                checked={columnVisibility[column.id]}
-                onCheckedChange={(value) =>
-                  handleVisibilityChange(column.id, !!value)
-                }
+                checked={column.getIsVisible()}
+                onCheckedChange={(value) => column.toggleVisibility(!!value)}
               >
                 {snakeToTitle(column.id)}
               </DropdownMenuCheckboxItem>
