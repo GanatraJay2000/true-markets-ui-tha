507b2bbdfa6a5c1841ce5234d0879c126337eebf
Jay Ganatra <ganatrajay2000@gmail.com>
2024-11-12 23:34:01 -0500

    remove logs

--- src/components/DataTable/FacetedFilter.tsx
+++ src/components/DataTable/FacetedFilter.tsx
@@ -33,7 +33,6 @@ export function DataTableFacetedFilter<TData, TValue>({
 }: DataTableFacetedFilterProps<TData, TValue>) {
   const facets = column?.getFacetedUniqueValues();
   const { tickers, currentTickers, setCurrentTickers } = useSocket();
-  console.log(currentTickers);
   return (
     <Popover>
       <PopoverTrigger asChild>
