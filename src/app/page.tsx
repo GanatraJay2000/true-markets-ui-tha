7954c4357a38cae94e78b8fb49fa9adeffb2b066
Jay Ganatra <ganatrajay2000@gmail.com>
2024-11-13 23:26:51 -0500

    reset button responsive fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import DataTableComponent from "@/components/DataTable/DataTable";
 export default function Home() {
   return (
     <div className="h-full w-full flex justify-center py-10 md:py-16 px-2 md:px-0 ">
-      <div className="md:w-3/4 max-w-[95%]">
+      <div className="md:w-3/4 max-w-[96%]">
         <DataTableComponent />
       </div>
     </div>
--- src/components/DataTable/Toolbar.tsx
+++ src/components/DataTable/Toolbar.tsx
@@ -19,38 +19,53 @@ export function DataTableToolbar<TData>({
 }: DataTableToolbarProps<TData>) {
   const { filtered, setCurrentTickers, tickers } = useSocket();
   return (
-    <div className="flex items-center justify-between">
-      <div className="flex flex-1 items-center space-x-2">
-        <Input
-          placeholder="Filter Product Ids..."
-          value={
-            (table.getColumn("product_id")?.getFilterValue() as string) ?? ""
-          }
-          onChange={(event) =>
-            table.getColumn("product_id")?.setFilterValue(event.target.value)
-          }
-          className="h-8 w-[150px] lg:w-[250px]  border-neutral-700"
-        />
-        <DataTableFacetedFilter
-          column={table.getColumn("product_id")}
-          title="Tickers"
-        />
+    <div className="space-y-2 md:space-y-0">
+      <div className="flex items-center justify-between space-x-2">
+        <div className="flex flex-1 items-center space-x-2">
+          <Input
+            placeholder="Filter Product Ids..."
+            value={
+              (table.getColumn("product_id")?.getFilterValue() as string) ?? ""
+            }
+            onChange={(event) =>
+              table.getColumn("product_id")?.setFilterValue(event.target.value)
+            }
+            className="h-8 w-[150px] lg:w-[250px]  border-neutral-700"
+          />
+          <DataTableFacetedFilter
+            column={table.getColumn("product_id")}
+            title="Tickers"
+          />
 
-        {filtered && (
-          <Button
-            variant="ghost"
-            onClick={() => {
-              table.resetColumnFilters();
-              setCurrentTickers(new Set(tickers));
-            }}
-            className="h-8 px-2 lg:px-3"
-          >
-            Reset
-            <X />
-          </Button>
-        )}
+          {filtered && (
+            <Button
+              variant="ghost"
+              onClick={() => {
+                table.resetColumnFilters();
+                setCurrentTickers(new Set(tickers));
+              }}
+              className="h-8 px-2 lg:px-3 hidden md:flex"
+            >
+              Reset
+              <X />
+            </Button>
+          )}
+        </div>
+        <DataTableViewOptions table={table} />
       </div>
-      <DataTableViewOptions table={table} />
+      {filtered && (
+        <Button
+          variant="outline"
+          onClick={() => {
+            table.resetColumnFilters();
+            setCurrentTickers(new Set(tickers));
+          }}
+          className="h-8 px-2 lg:px-3 w-full border-dashed border-neutral-700 md:hidden"
+        >
+          Reset
+          <X />
+        </Button>
+      )}
     </div>
   );
 }
